5593e631f4040dea7d9ecc538534e70ea1e9ab0e
victor.zaragoza <victor.zaragoza@exponentiateam.com>
2020-10-21 19:09:44 +0200

    controls and creator refactor

--- src/components/SchemaCreator/SchemaControls.tsx
+++ src/components/SchemaCreator/SchemaControls.tsx
@@ -1,40 +1,65 @@
 import React from 'react'
+import _ from 'lodash/fp'
+
 import { Input } from '../Input'
 import { SchemaTypesSelect } from '../Select'
 import * as helpers from '../../utils/helpers'
-import { Schema} from '../../types'
+import { Schema } from '../../utils/types'
 import { AddButton, DeleteButton } from '../Buttons'
 
 type Props = {
   schema: Schema
   onDelete?: () => void
   onAdd?: () => void
-  onChange: (key: string, value: unknown) => void
+  onChange: (schema: Schema) => void
 }
 
-const SchemaControls: React.FunctionComponent<Props> = ({
+export const SchemaControls: React.FunctionComponent<Props> = ({
   schema,
   onDelete,
-  onAdd,
-  onChange
+  onChange,
+  onAdd
 }: Props) => {
   return (
     <div className='grid gap-2 grid-flow-col items-center'>
       <Input
         value={helpers.getSchemaTitle(schema)}
-        onChange={(t) => onChange('title', t)}
+        onChange={(t) => onChange(helpers.setSchemaTitle(t, schema))}
         placeholder='Title'
       />
       <SchemaTypesSelect
         type={helpers.getSchemaType(schema)}
-        onChange={(t) => onChange('type', t)}
+        onChange={(t) => onChange(helpers.setSchemaType(t, schema))}
       />
       <div className='grid grid-flow-col items-center gap-1'>
-        {onDelete && <DeleteButton onClick={onDelete} />}
-        {onAdd && <AddButton onClick={onAdd} />}
+        {_.isFunction(onDelete) && <DeleteButton onClick={onDelete} />}
+        {_.isFunction(onAdd) && <AddButton onClick={onAdd} />}
       </div>
     </div>
   )
 }
 
-export default SchemaControls
+type ArrayProps = {
+  schema: Schema
+  onChange: (schema: Schema) => void
+  onAdd?: () => void
+}
+
+export const SchemaArrayControls: React.FunctionComponent<ArrayProps> = ({
+  schema,
+  onChange,
+  onAdd
+}: ArrayProps) => {
+  return (
+    <div className='flex items-center'>
+      <label className='mr-2'>Items</label>
+      <SchemaTypesSelect
+        type={helpers.getSchemaType(schema)}
+        onChange={(t) => onChange(helpers.setSchemaType(t, schema))}
+      />
+      <div className='ml-2 grid grid-flow-col items-center gap-1'>
+        {_.isFunction(onAdd) && <AddButton onClick={onAdd} />}
+      </div>
+    </div>
+  )
+}
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,7 +1,8 @@
 import _ from 'lodash/fp'
-import { Schema} from '../types'
+import { Schema} from './types'
 
 export const getSchemaField = _.get
+export const getSchemaFields = _.pick
 export const getSchemaType = getSchemaField('type')
 export const getSchemaTitle = getSchemaField('title')
 export const getSchemaProperty = (key: string) => getSchemaField(['properties', key])
@@ -14,17 +15,23 @@ export const setSchemaTitle = setSchemaField('title')
 export const setSchemaProperty = (key: string) => setSchemaField(['properties', key])
 export const setSchemaItems = setSchemaField('items')
 
+export const mergeSchemaFields = _.merge
+
+export const cleanSchemaFields = (schema: Schema) => {
+  console.log(schema)
+  //getSchemaFields(getSchemaAvailableFieldsByType(getSchemaType(schema)), schema)
+
+}
 
 export const deleteSchemaProperty = (key: string) => _.omit([`properties.${key}`])
-export const addSchemaProperty = (schema: Schema) => setSchemaProperty(`__${Date.now()}__`)({}, schema)
+export const addSchemaProperty = (schema: Schema) => setSchemaProperty(`__${_.now()}__`)({}, schema)
 
 export const isSchemaObject = (schema: Schema) => getSchemaType(schema) === 'object'
 export const isSchemaArray = (schema: Schema) => getSchemaType(schema) === 'array'
-export const hasSchemaProperties= (schema: Schema) => !_.isEmpty(getSchemaProperties(schema))
+export const hasSchemaProperties = (schema: Schema) => !_.isEmpty(getSchemaProperties(schema))
+export const hasSchemaItems = (schema: Schema) => !_.isEmpty(getSchemaItems(schema))
 
-export const stringToKey = (text: string) => {
-  return text?.trim().normalize("NFD").replace(/[\u0300-\u036f]/g, '').replace(/\s/g, "_").toLowerCase()
-}
+export const stringToKey = _.snakeCase
 
 export const findOption = (value: string) => _.find(['value', value])
 
