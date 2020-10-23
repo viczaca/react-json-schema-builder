eb1efdc3f249f55c2914827832d7bf9fcd5d2659
victor.zaragoza <victor.zaragoza@exponentiateam.com>
2020-10-23 15:22:33 +0200

    more helpers

--- src/tests/helpers.test.ts
+++ src/tests/helpers.test.ts
@@ -1,6 +1,31 @@
+import {
+  arraySchemaOptions,
+  arrayValidSchemaProperties,
+  boolSchemaOptions,
+  boolValidSchemaProperties,
+  integerSchemaOptions,
+  integerValidSchemaProperties,
+  numberSchemaOptions,
+  numberValidSchemaProperties,
+  objectSchemaOptions,
+  objectValidSchemaProperties,
+  stringSchemaOptions,
+  stringValidSchemaProperties
+} from '../utils/constants'
 import * as helpers from '../utils/helpers'
 
 describe('schema', () => {
+  test('get all schema keys', () => {
+    const schema = {
+      type: 'string',
+      description: 'Something'
+    }
+
+    const result = helpers.getAllSchemaKeys(schema)
+
+    expect(result).toEqual(['type', 'description'])
+  })
+
   test('get schema field', () => {
     const desc = 'Something description'
     const schema = {
@@ -11,6 +36,20 @@ describe('schema', () => {
     const result = helpers.getSchemaField('description', schema)
 
     expect(result).toBe(desc)
+  })
+
+  test('get schema fields', () => {
+    const schemaBefore = {
+      type: 'string',
+      description: 'Something description'
+    }
+    const schemaAfter = {
+      type: 'string'
+    }
+
+    const result = helpers.getSchemaFields(['type'], schemaBefore)
+
+    expect(result).toEqual(schemaAfter)
   })
 
   test('get schema type', () => {
@@ -80,8 +119,49 @@ describe('schema', () => {
     expect(result).toEqual(items)
   })
 
+  test('get schema required', () => {
+    const schema = {
+      type: 'object',
+      required: ['name'],
+      properties: {
+        name: {
+          type: 'string'
+        }
+      }
+    }
+    const required = ['name']
+
+    const result = helpers.getSchemaRequired(schema)
+
+    expect(result).toEqual(required)
+  })
+
+  test('get schema required properties', () => {
+    const schema = {
+      type: 'object',
+      required: ['name'],
+      properties: {
+        name: {
+          type: 'string'
+        },
+        surname: {
+          type: 'string'
+        }
+      }
+    }
+    const properties = {
+      name: {
+        type: 'string'
+      }
+    }
+
+    const result = helpers.getSchemaRequiredProperties(schema)
+
+    expect(result).toEqual(properties)
+  })
+
   test('set schema field', () => {
-    const desc = "Something description"
+    const desc = 'Something description'
     const schema = {
       type: 'string',
       description: 'Desc'
@@ -115,6 +195,26 @@ describe('schema', () => {
     expect(helpers.getSchemaTitle(result)).toBe(title)
   })
 
+  test('set schema properties', () => {
+    const schema = {
+      type: 'object',
+      properties: {
+        name: {
+          type: 'string'
+        }
+      }
+    }
+    const properties = {
+      hello: {
+        type: 'string'
+      }
+    }
+
+    const result = helpers.setSchemaProperties(properties, schema)
+
+    expect(helpers.getSchemaProperties(result)).toEqual(properties)
+  })
+
   test('set schema property', () => {
     const schema = {
       type: 'object',
@@ -129,6 +229,36 @@ describe('schema', () => {
     const result = helpers.setSchemaProperty('name')(property, schema)
 
     expect(helpers.getSchemaProperty('name')(result)).toBe(property)
+  })
+
+  test('set schema items', () => {
+    const schema = {
+      type: 'array',
+      items: {
+        type: 'string'
+      }
+    }
+    const items = {
+      type: 'integer'
+    }
+
+    const result = helpers.setSchemaItems(items, schema)
+
+    expect(helpers.getSchemaItems(result)).toEqual(items)
+  })
+
+  test('delete schema field', () => {
+    const schemaBefore = {
+      type: 'string',
+      description: 'Something'
+    }
+    const schemaAfter = {
+      type: 'string'
+    }
+
+    const result = helpers.deleteSchemaField('description')(schemaBefore)
+
+    expect(result).toEqual(schemaAfter)
   })
 
   test('delete schema property', () => {
@@ -168,6 +298,53 @@ describe('schema', () => {
     expect(helpers.getSchemaProperties(result)).not.toEqual({})
   })
 
+  test('rename schema field', () => {
+    const schemaBefore = {
+      type: 'object',
+      description: 'Something'
+    }
+    const schemaAfter = {
+      type: 'object',
+      updated: 'Something'
+    }
+
+    const result = helpers.renameSchemaField(
+      'description',
+      'updated'
+    )(schemaBefore)
+
+    expect(result).toEqual(schemaAfter)
+  })
+
+  test('rename schema property', () => {
+    const schemaBefore = {
+      type: 'object',
+      properties: {
+        name: {
+          type: 'string'
+        },
+        surname: {
+          type: 'string'
+        }
+      }
+    }
+    const schemaAfter = {
+      type: 'object',
+      properties: {
+        updated: {
+          type: 'string'
+        },
+        surname: {
+          type: 'string'
+        }
+      }
+    }
+
+    const result = helpers.renameSchemaProperty('name', 'updated', schemaBefore)
+
+    expect(result).toEqual(schemaAfter)
+  })
+
   test('is schema object', () => {
     const result_1 = helpers.isSchemaObject({
       type: 'object'
@@ -192,6 +369,27 @@ describe('schema', () => {
     expect(result_2).toBe(false)
   })
 
+  test('is field required', () => {
+    const schema = {
+      type: 'object',
+      required: ['name'],
+      properties: {
+        name: {
+          type: 'string'
+        },
+        surname: {
+          type: 'string'
+        }
+      }
+    }
+
+    const result_1 = helpers.isFieldRequired('name')(schema)
+    const result_2 = helpers.isFieldRequired('surname')(schema)
+
+    expect(result_1).toBe(true)
+    expect(result_2).toBe(false)
+  })
+
   test('has schema properties', () => {
     const result_1 = helpers.hasSchemaProperties({
       type: 'object',
@@ -210,18 +408,200 @@ describe('schema', () => {
     expect(result_2).toBe(false)
   })
 
-})
+  test('has schema items', () => {
+    const result_1 = helpers.hasSchemaItems({
+      type: 'array',
+      items: {
+        type: 'string'
+      }
+    })
+    const result_2 = helpers.hasSchemaItems({
+      type: 'array',
+      items: {}
+    })
 
-describe('common', () => {
-  test('create correct key from string', () => {
-    const text = 'Náme of the Key '
-
-    const result = helpers.stringToKey(text)
-
-    expect(result).toBe('name_of_the_key')
+    expect(result_1).toBe(true)
+    expect(result_2).toBe(false)
   })
 
-  test('finds correct element in option list', () => {
+  test('get schema menu options', () => {
+    const result_1 = helpers.getSchemaMenuOptions('string')
+    const result_2 = helpers.getSchemaMenuOptions('integer')
+    const result_3 = helpers.getSchemaMenuOptions('number')
+    const result_4 = helpers.getSchemaMenuOptions('boolean')
+    const result_5 = helpers.getSchemaMenuOptions('object')
+    const result_6 = helpers.getSchemaMenuOptions('array')
+
+    expect(result_1).toEqual(stringSchemaOptions)
+    expect(result_2).toEqual(integerSchemaOptions)
+    expect(result_3).toEqual(numberSchemaOptions)
+    expect(result_4).toEqual(boolSchemaOptions)
+    expect(result_5).toEqual(objectSchemaOptions)
+    expect(result_6).toEqual(arraySchemaOptions)
+  })
+
+  test('options to strings', () => {
+    const options = [
+      { value: 'name', label: 'Name' },
+      { value: 'surname', label: 'surname' }
+    ]
+    const strings = ['name', 'surname']
+
+    const result = helpers.optionsToStrings(options)
+
+    expect(result).toEqual(strings)
+  })
+
+  test('strings to options', () => {
+    const strings = ['name', 'surname']
+    const options = [
+      { value: 'name', label: 'name' },
+      { value: 'surname', label: 'surname' }
+    ]
+
+    const result = helpers.stringsToOptions(strings)
+
+    expect(result).toEqual(options)
+  })
+
+  test('schema field as option', () => {
+    const result_1 = helpers.schemaFieldAsOption('name', {
+      title: 'Naming',
+      type: 'string'
+    })
+    const result_2 = helpers.schemaFieldAsOption('surname', {
+      type: 'string'
+    })
+
+    expect(result_1).toEqual({ value: 'name', label: 'Naming' })
+    expect(result_2).toEqual({ value: 'surname', label: 'surname' })
+  })
+
+  test('fields to options', () => {
+    const schema = {
+      name: {
+        title: 'Naming',
+        type: 'string'
+      },
+      surname: {
+        type: 'string'
+      }
+    }
+    const options = [
+      { value: 'name', label: 'Naming' },
+      { value: 'surname', label: 'surname' }
+    ]
+
+    const result = helpers.fieldsToOptions(schema)
+
+    expect(result).toEqual(options)
+  })
+
+  test('schema properties as options', () => {
+    const schema = {
+      type: 'object',
+      properties: {
+        name: {
+          title: 'Name',
+          type: 'string'
+        },
+        surname: {
+          type: 'string'
+        }
+      }
+    }
+    const options = [
+      { value: 'name', label: 'Name' },
+      { value: 'surname', label: 'surname' }
+    ]
+
+    const result = helpers.schemaPropertiesAsOptions(schema)
+
+    expect(result).toEqual(options)
+  })
+
+  test('schema required properties as options', () => {
+    const schema = {
+      type: 'object',
+      required: ['name'],
+      properties: {
+        name: {
+          title: 'Naming',
+          type: 'string'
+        },
+        surname: {
+          type: 'string'
+        }
+      }
+    }
+    const options = [{ value: 'name', label: 'Naming' }]
+
+    const result = helpers.schemaRequiredPropertiesAsOptions(schema)
+
+    expect(result).toEqual(options)
+  })
+
+  test('get valid fields', () => {
+    const result_1 = helpers.getValidFields('string')
+    const result_2 = helpers.getValidFields('integer')
+    const result_3 = helpers.getValidFields('number')
+    const result_4 = helpers.getValidFields('boolean')
+    const result_5 = helpers.getValidFields('object')
+    const result_6 = helpers.getValidFields('array')
+
+    expect(result_1).toEqual(stringValidSchemaProperties)
+    expect(result_2).toEqual(integerValidSchemaProperties)
+    expect(result_3).toEqual(numberValidSchemaProperties)
+    expect(result_4).toEqual(boolValidSchemaProperties)
+    expect(result_5).toEqual(objectValidSchemaProperties)
+    expect(result_6).toEqual(arrayValidSchemaProperties)
+  })
+
+  test('remove wrong fields', () => {
+    const schemaBefore = {
+      type: 'string',
+      required: ['name'],
+      description: 'Something',
+      properties: {
+        name: {
+          title: 'Naming',
+          type: 'string'
+        },
+        surname: {
+          type: 'string'
+        }
+      }
+    }
+
+    const schemaAfter = {
+      type: 'string',
+      description: 'Something'
+    }
+
+    const result = helpers.removeWrongFields(schemaBefore)
+
+    expect(result).toEqual(schemaAfter)
+  })
+
+  test('set schema type and remove wrong fields', () => {
+    const schemaBefore = {
+      type: 'integer',
+      minimum: 3,
+      maximum: 5,
+      description: 'Something',
+    }
+
+    const schemaAfter = {
+      type: 'object',
+      description: 'Something',
+    }
+
+    const result = helpers.setSchemaTypeAndRemoveWrongFields('object', schemaBefore)
+
+    expect(result).toEqual(schemaAfter)
+  })
+
+  test('find option', () => {
     const options = [
       {
         value: 'hello',
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,45 +1,125 @@
 import _ from 'lodash/fp'
-import { typeToOptions } from './constants'
+import { typeToOptions, typeToValidFields } from './constants'
 import { Schema, SchemaType } from './types'
 
-export const getAllSchemaFields = _.keys
+export const getAllSchemaKeys = _.keys
+
 export const getSchemaField = _.get
+
+export const getSchemaFields = _.pick
+
 export const getSchemaType = getSchemaField('type')
+
 export const getSchemaTitle = getSchemaField('title')
+
 export const getSchemaProperty = (key: string) =>
   getSchemaField(['properties', key])
+
 export const getSchemaProperties = getSchemaField('properties')
+
 export const getSchemaItems = getSchemaField('items')
 
+export const getSchemaRequired = getSchemaField('required')
+
+export const getSchemaRequiredProperties = (schema: Schema) => {
+  const required = getSchemaRequired(schema)
+  const properties = getSchemaProperties(schema)
+  return getSchemaFields(required, properties)
+}
+
 export const setSchemaField = _.set
+
 export const setSchemaType = setSchemaField('type')
+
 export const setSchemaTitle = setSchemaField('title')
+
+export const setSchemaProperties = setSchemaField('properties')
+
 export const setSchemaProperty = (key: string) =>
   setSchemaField(['properties', key])
+
 export const setSchemaItems = setSchemaField('items')
 
+export const deleteSchemaField = _.unset
 
-export const deleteSchemaField = _.omit
 export const deleteSchemaProperty = (key: string) =>
-  deleteSchemaField([`properties.${key}`])
+  deleteSchemaField(['properties', key])
+
 export const addSchemaProperty = (schema: Schema) =>
   setSchemaProperty(`__${_.now()}__`)({}, schema)
 
+export const renameSchemaField = (oldKey: string, newKey: string) =>
+  //Rename field key but maintains object "order"
+  _.flow([
+    _.entries,
+    _.map(([k, v]) => ({ [k === oldKey ? newKey : k]: v })),
+    _.reduce(_.assign, {})
+  ])
+
+export const renameSchemaProperty = (
+  oldKey: string,
+  newKey: string,
+  schema: Schema
+) =>
+  _.flow([
+    getSchemaProperties,
+    renameSchemaField(oldKey, newKey),
+    (p) => setSchemaProperties(p, schema)
+  ])(schema)
+
 export const isSchemaObject = (schema: Schema) =>
   getSchemaType(schema) === 'object'
+
 export const isSchemaArray = (schema: Schema) =>
   getSchemaType(schema) === 'array'
+
+export const isFieldRequired = (key: string) =>
+   _.flow([getSchemaRequired, _.contains(key)])
+
 export const hasSchemaProperties = (schema: Schema) =>
   !_.isEmpty(getSchemaProperties(schema))
+
 export const hasSchemaItems = (schema: Schema) =>
   !_.isEmpty(getSchemaItems(schema))
 
 export const getSchemaMenuOptions = (type: SchemaType) =>
   _.get(type, typeToOptions)
 
-export const stringToKey = _.snakeCase
-
 export const findOption = (value: string) => _.find(['value', value])
 
 export const optionsToStrings = _.map('value')
-export const stringsToOptions = _.map((s) => ({label: s, value: s}))
+
+export const stringsToOptions = _.map((s) => ({ label: s, value: s }))
+
+export const schemaFieldAsOption = (key: string, schema: Schema) => {
+  const title = getSchemaTitle(schema)
+  if (!_.isEmpty(title)) {
+    return { value: key, label: title }
+  }
+  return { value: key, label: key }
+}
+
+export const fieldsToOptions = _.flow([
+  _.entries,
+  _.map(([key, val]) => schemaFieldAsOption(key, val))
+])
+
+export const schemaPropertiesAsOptions = _.flow([
+  getSchemaProperties,
+  fieldsToOptions
+])
+
+export const schemaRequiredPropertiesAsOptions = _.flow(
+  getSchemaRequiredProperties,
+  fieldsToOptions
+)
+
+export const getValidFields = (type: SchemaType) => _.get(type, typeToValidFields)
+
+export const removeWrongFields = (schema: Schema) => {
+  const type = getSchemaType(schema)
+  const fields = getValidFields(type)
+  return getSchemaFields(fields, schema)
+}
+
+export const setSchemaTypeAndRemoveWrongFields = _.flow([setSchemaType, removeWrongFields])
