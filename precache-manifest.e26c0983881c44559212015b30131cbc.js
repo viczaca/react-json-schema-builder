a99ecad34b8d091267ccec6abc17fcf36a09ec18
victor.zaragoza <victor.zaragoza@exponentiateam.com>
2020-10-26 11:16:11 +0100

    Updates

---
+++ precache-manifest.e26c0983881c44559212015b30131cbc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "001d84da9c1e342e8fc06c623b721ea2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c5b8d45743d55ea85203",
+    "url": "./static/css/2.01fc9a20.chunk.css"
+  },
+  {
+    "revision": "3fc78f1d8026dcc7f142",
+    "url": "./static/css/main.5e9af68b.chunk.css"
+  },
+  {
+    "revision": "c5b8d45743d55ea85203",
+    "url": "./static/js/2.4bec46fb.chunk.js"
+  },
+  {
+    "revision": "e9ba443c9b39a1993e82b561e4d9600f",
+    "url": "./static/js/2.4bec46fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fc78f1d8026dcc7f142",
+    "url": "./static/js/main.b969a209.chunk.js"
+  },
+  {
+    "revision": "9a514b5d834cbba04ad6",
+    "url": "./static/js/runtime-main.90ee6e7e.js"
+  }
+]);
